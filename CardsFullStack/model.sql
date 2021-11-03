feed41dfa2e4d62e0bf49327f13cd33d865393d7
Tristin Bruyere <TristinBruyere@quickenloans.com>
2021-11-03 11:18:41 -0400

    Updated DB

--- CardsFullStack/model.sql
+++ CardsFullStack/model.sql
@@ -1,1 +1,21 @@
-﻿
+﻿create database CardGame;
+
+use CardGame;
+
+create table Deck (
+    deck_id varchar(20),
+    is_current boolean,
+    created_at DateTime,
+    username varchar(20),
+    PRIMARY KEY (deck_id)
+);
+
+create table Card (
+	id int not null auto_increment,
+	deck_id varchar(20),
+    image varchar(200),
+    cardcode varchar(3),
+    username varchar(20),
+    created_at datetime,
+    PRIMARY KEY (id)
+);
