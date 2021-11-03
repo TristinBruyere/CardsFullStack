5d7634192e77a96cc4456e7cb939e4044485e478
Tristin Bruyere <TristinBruyere@quickenloans.com>
2021-11-03 11:43:59 -0400

    Created response model

---
+++ CardsFullStack/Models/DeckResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardsFullStack.Models
+{ // This class gets a response from the API
+    public class DeckResponse
+    {
+        public string deck_id { get; set; }
+        public int remaining { get; set; }
+
+    }
+}
